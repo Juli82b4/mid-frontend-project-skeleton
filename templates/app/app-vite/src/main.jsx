2bde09a8c86b035329dae1bac524ba3065e770fa
shpomp <karolina.urnieziute@gmail.com>
2026-03-03 00:16:17 +0100

    [feat/fe-skeleton] - auth context

--- templates/app/app-vite/src/main.jsx
+++ templates/app/app-vite/src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout/Layout.jsx";
 import HomePage from "./components/HomePage/HomePage.jsx";
 import EventList from "./components/EventList/EventList.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import "./main.css";
 
 const router = createBrowserRouter([
@@ -20,6 +21,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 );
